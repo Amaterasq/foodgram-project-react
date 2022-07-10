59bc5b8deaa54a0e2a20fa89cad88eb5cf82c19e
Amaterasq <perepechcko.vlad@ya.ru>
2022-07-10 17:37:23 +0300

    filter author add

--- backend/foodgram_project/api/filters.py
+++ backend/foodgram_project/api/filters.py
@@ -1,7 +1,7 @@
 from django_filters.rest_framework import FilterSet, filters
 from rest_framework.filters import SearchFilter
 
-from api.models import Recipe
+from api.models import Recipe, User
 
 
 class IngredientSearchFilter(SearchFilter):
@@ -9,6 +9,7 @@ class IngredientSearchFilter(SearchFilter):
 
 
 class RecipeFilter(FilterSet):
+    author = filters.ModelChoiceFilter(queryset=User.objects.all())
     tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
     is_favorited = filters.BooleanFilter(method='filter_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
